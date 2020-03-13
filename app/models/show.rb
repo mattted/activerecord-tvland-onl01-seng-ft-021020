fa8f4eee5dae433230dfebf07a511d1b8602f395
mattted <medward4@gmail.com>
2020-03-13 13:38:12 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def actors_list
+    self.characters.map{ |c| c.actor.full_name }
+  end
+
 end
---
+++ db/migrate/006_add_character_catchphrase.rb
@@ -0,0 +1,7 @@
+class AddCharacterCatchphrase < ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :characters, :catchphrase, :string
+  end
+
+end
